76ee323da01ac969cd133d93e3b19d982b881a72
lilianelou <lilianelou@hotmail.com>
2023-09-13 08:22:04 -0300

    feat: finaliza avaliação

---
+++ assets/dados_categorias.py
@@ -0,0 +1,12 @@
+dados_categorias = [
+    {'nome': 'Eletrônicos'},
+    {'nome': 'Roupas'},
+    {'nome': 'Calçados'},
+    {'nome': 'Livros e Papelaria'},
+    {'nome': 'Esportes e Lazer'},
+    {'nome': 'Móveis'},
+    {'nome': 'Eletrodomésticos'},
+    {'nome': 'Acessórios'},
+    {'nome': 'Periféricos de Computador'}
+]
+
